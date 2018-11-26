1db1e0a372c7a42a38263bf20fe6dffa99ca063f
Minh Le <workonly140287@gmail.com>
2018-11-26 15:13:04 -0500

    Initial Commit

---
+++ taximeter2/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  taximeter2
+//
+//  Created by Minh Le on 2018-11-26.
+//  Copyright © 2018 Minh Le. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
